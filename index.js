20e244c411f7a2aee1b05a38fdc97cc28eb3834f
Vladimir Iliakov <vladimir.iliakov@suse.com>
2025-10-16 09:00:20 +0200

    part3-3

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require('express')
 var morgan = require('morgan')
 
 const app = express()
-const cors = require('cors')
 
 app.use(express.static('dist'))
-app.use(cors())
 app.use(express.json())
 app.use(morgan(function (tokens, request, response) {
     const body = JSON.stringify(request.body)
