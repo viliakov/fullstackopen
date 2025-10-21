827739692d84c4dc33e12d617e3133077a80a0cd
Vladimir Iliakov <vladimir.iliakov@suse.com>
2025-10-21 08:50:03 +0200

    part3-4

--- index.js
+++ index.js
@@ -8,103 +8,102 @@ const app = express()
 const Contact = require('./models/contact')
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(function (tokens, request, response) {
-    const body = JSON.stringify(request.body)
-    return [
-        tokens.method(request, response),
-        tokens.url(request, response),
-        tokens.status(request, response),
-        tokens.res(request, response, 'content-length'), '-',
-        tokens['response-time'](request, response), 'ms',
-        body,
-    ].join(' ')
+  const body = JSON.stringify(request.body)
+  return [
+    tokens.method(request, response),
+    tokens.url(request, response),
+    tokens.status(request, response),
+    tokens.res(request, response, 'content-length'), '-',
+    tokens['response-time'](request, response), 'ms',
+    body,
+  ].join(' ')
 }))
 
 app.get('/api/contacts', (request, response) => {
 
-    Contact.find({}).then(contacts => {
-        response.json(contacts)
-    })
+  Contact.find({}).then(contacts => {
+    response.json(contacts)
+  })
 })
 
 app.get('/info', (request, response) => {
-    const currentTime = new Date()
-    Contact.find({}).then(contacts => {
-        response.send(`
+  const currentTime = new Date()
+  Contact.find({}).then(contacts => {
+    response.send(`
             <p>Phonebook has info for ${contacts.length} people</p>
             <p>${currentTime.toString()}</p>
         `)
-    })
+  })
 })
 
 app.get('/api/contacts/:id', (request, response, next) => {
-    Contact.findById(request.params.id).then(contact => {
-        if (contact) {
-            response.json(contact)
-        } else {
-            response.status(404).end()
-        }
-    }).catch(error => next(error))
+  Contact.findById(request.params.id).then(contact => {
+    if (contact) {
+      response.json(contact)
+    } else {
+      response.status(404).end()
+    }
+  }).catch(error => next(error))
 })
 
 app.delete('/api/contacts/:id', (request, response, next) => {
-    Contact.findByIdAndDelete(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Contact.findByIdAndDelete(request.params.id)
+    .then(
+      response.status(204).end()
+    )
+    .catch(error => next(error))
 })
 
 app.post('/api/contacts', (request, response,next) => {
-    const body = request.body
+  const body = request.body
 
-    const contact = new Contact({
-        name: body.name,
-        number: body.number,
-    })
+  const contact = new Contact({
+    name: body.name,
+    number: body.number,
+  })
 
-    contact.save().then(savedContact => {
-        response.json(savedContact)
-    })
+  contact.save().then(savedContact => {
+    response.json(savedContact)
+  })
     .catch(error => next(error))
 })
 
 app.put('/api/contacts/:id', (request, response, next) => {
-    const { name, number } = request.body
-    const opts = { runValidators: true };
+  const { name, number } = request.body
 
-    Contact.findById(request.params.id)
-        .then(contact => {
-            if (!contact) {
-                return response.status(404).end()
-            }
+  Contact.findById(request.params.id)
+    .then(contact => {
+      if (!contact) {
+        return response.status(404).end()
+      }
 
-            contact.name = name
-            contact.number = number
+      contact.name = name
+      contact.number = number
 
-            return contact.save().then((updatedContact) => {
-                response.json(updatedContact)
-            })
-        })
-        .catch(error => next(error))
+      return contact.save().then((updatedContact) => {
+        response.json(updatedContact)
+      })
+    })
+    .catch(error => next(error))
 })
 
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unknown endpoint'})
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
@@ -113,5 +112,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
